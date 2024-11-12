85b9152edc82fe8b73d8fb6d12f11487f3e963b5
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-11-12 15:39:26 -0500

    replace with react skeleton

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <hgroup>
+        <h1>MostlyFocused On Data</h1>
+        <p>Traffic Analysis for my site</p>
+      </hgroup>
+    </main>
+  )
+}
+
+export default App
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-const opts = {
-  build: { outDir: 'build' },
-};
-
-export default defineConfig(opts)
