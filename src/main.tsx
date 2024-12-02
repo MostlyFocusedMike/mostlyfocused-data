56d898a0d65b6a913e963e7ed3eb94078cb92fcb
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-12-01 23:35:26 -0500

    add in react router for query params

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './init-chart';
+import { BrowserRouter, Route, Routes } from 'react-router'
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
